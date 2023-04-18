04368f8ce3288f67080ba55d2cac6cee83c2de84
Dominique Clark <melaydominique@gmail.com>
2023-04-17 20:56:47 -0400

    map, switched around deck function

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -3,20 +3,24 @@ import { Routes, Route, useParams } from "react-router-dom";
 // import Deck from "./Deck";
 
 const Deck = (props) => {
-  // useEffect(() => {
-  //   props.getDecks()
-  // })
-  // console.log(props.deck);
+  const [decks, setDecks] = useState([])
 
+  const getDecks = () => {
+    axios.get('http://localhost:8000/api/decks').then((response) => {
+      setDecks(response.data)
+    })
+  }
+  {decks.map((deck) => {
   return (
     <div className="deckdiv">
-      <h3>{props.deck.title}</h3>
-      <h4>Subject: {props.deck.subject}</h4>
-      <h4>Class: {props.deck.classs}</h4>
-      <h4>Topic: {props.deck.topic}</h4>
-      <h5>Comments: {props.deck.comments}</h5>
+      <h3>{deck.title}</h3>
+      <h4>Subject: {deck.subject}</h4>
+      <h4>Class: {deck.classs}</h4>
+      <h4>Topic: {deck.topic}</h4>
+      <h5>Comments: {deck.comments}</h5>
     </div>
   );
+})}
 };
 
 export default Deck;
